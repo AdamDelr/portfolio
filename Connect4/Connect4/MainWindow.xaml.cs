e576ced3ee1f4463b38f7ae0f7ed782a03e3bfca
Adam delRosario <AdamdelRosario@gmail.com>
2017-09-08 17:28:44 -0700

    UI Backup

--- Connect4/Connect4/MainWindow.xaml.cs
+++ Connect4/Connect4/MainWindow.xaml.cs
@@ -24,5 +24,43 @@ namespace Connect4
         {
             InitializeComponent();
         }
+
+        private void button_A1_Click(object sender, RoutedEventArgs e)
+        {
+            Button A1 = sender as Button;
+            A1.Background = A1.Background == Brushes.Red ? (SolidColorBrush)
+                (new BrushConverter().ConvertFrom("SlateGray")): Brushes.Red;
+
+        }
+
+        private void button_A2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_A3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_A4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_A5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_A6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_A7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
