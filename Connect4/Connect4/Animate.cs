46fa187a05f403513f31ecf0242dca0aa511225c
AdamDelr <AdamdelRosario@gmail.com>
2017-09-14 23:21:18 -0700

    Added game piece animation

---
+++ Connect4/Connect4/Animate.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Connect4
+{
+    internal static class Animate
+    {
+        //List of timers to avoid garbage collection
+        private static IList<DispatcherTimer> timers = new List<DispatcherTimer>();
+
+        //Delay action
+        internal static void delayedExecute(Action action, double delay )
+        {
+            var dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
+            timers.Add(dispatcherTimer);
+
+            EventHandler handler = null;
+            handler = (sender, e) =>
+            {
+
+                dispatcherTimer.Tick -= handler;
+                dispatcherTimer.Stop();
+
+                timers.Remove(dispatcherTimer);
+
+                action();
+            };
+
+            dispatcherTimer.Tick += handler;
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(delay);
+            dispatcherTimer.Start();
+        }
+
+        //Set Button Color Functions ---------------------------------------------------------
+        internal static void setRed(Button button)
+        {
+            button.Background = button.Background == Brushes.Red ? (SolidColorBrush)
+                (new BrushConverter().ConvertFrom("SlateGray")) : Brushes.Red;
+        }
+
+        internal static void setBlue(Button button)
+        {
+            button.Background = button.Background == Brushes.Blue ? (SolidColorBrush)
+                (new BrushConverter().ConvertFrom("SlateGray")) : Brushes.Blue;
+        }
+
+        internal static void setGrey(Button button)
+        {
+            if (button.Background == Brushes.Red)
+            {
+                button.Background = button.Background == Brushes.SlateGray ? (SolidColorBrush)
+                (new BrushConverter().ConvertFrom("Red")) : Brushes.SlateGray;
+            }
+            else
+            {
+                button.Background = button.Background == Brushes.SlateGray ? (SolidColorBrush)
+                (new BrushConverter().ConvertFrom("Blue")) : Brushes.SlateGray;
+            }
+        }
+
+        internal static void dropPiece(Button A, Button B, Button C, Button D, Button E, Button F, Dictionary<string,bool> state)
+        {
+            if(state[A.Name])
+            {
+               //Column is full
+            }
+            else if (state[B.Name])
+            {
+                Animate.setRed(A);
+                state[A.Name] = true;
+            }
+            else if (state[C.Name])
+            {
+                Animate.setRed(A);
+                Animate.delayedExecute(() => Animate.setGrey(A), .4);
+
+                Animate.delayedExecute(() => Animate.setRed(B), .3);
+                state[B.Name] = true;
+            }
+            else if (state[D.Name])
+            {
+                Animate.setRed(A);
+                Animate.delayedExecute(() => Animate.setGrey(A), .4);
+
+                Animate.delayedExecute(() => Animate.setRed(B), .3);
+                Animate.delayedExecute(() => Animate.setGrey(B), .4);
+
+                Animate.delayedExecute(() => Animate.setRed(C), .35);
+                state[C.Name] = true;
+            }
+            else if (state[E.Name])
+            {
+                Animate.setRed(A);
+                Animate.delayedExecute(() => Animate.setGrey(A), .4);
+
+                Animate.delayedExecute(() => Animate.setRed(B), .3);
+                Animate.delayedExecute(() => Animate.setGrey(B), .4);
+
+                Animate.delayedExecute(() => Animate.setRed(C), .35);
+                Animate.delayedExecute(() => Animate.setGrey(C), .45);
+
+                Animate.delayedExecute(() => Animate.setRed(D), .4);
+                state[D.Name] = true;
+            }
+            else if (state[F.Name])
+            {
+                Animate.setRed(A);
+                Animate.delayedExecute(() => Animate.setGrey(A), .4);
+
+                Animate.delayedExecute(() => Animate.setRed(B), .3);
+                Animate.delayedExecute(() => Animate.setGrey(B), .4);
+
+                Animate.delayedExecute(() => Animate.setRed(C), .35);
+                Animate.delayedExecute(() => Animate.setGrey(C), .45);
+
+                Animate.delayedExecute(() => Animate.setRed(D), .4);
+                Animate.delayedExecute(() => Animate.setGrey(D), .5);
+
+                Animate.delayedExecute(() => Animate.setRed(E), .45);
+                state[E.Name] = true;
+            }
+            else 
+            {
+                Animate.setRed(A);
+                Animate.delayedExecute(() => Animate.setGrey(A), .4);
+
+                Animate.delayedExecute(() => Animate.setRed(B), .3);
+                Animate.delayedExecute(() => Animate.setGrey(B), .4);
+
+                Animate.delayedExecute(() => Animate.setRed(C), .35);
+                Animate.delayedExecute(() => Animate.setGrey(C), .45);
+
+                Animate.delayedExecute(() => Animate.setRed(D), .4);
+                Animate.delayedExecute(() => Animate.setGrey(D), .5);
+
+                Animate.delayedExecute(() => Animate.setRed(E), .45);
+                Animate.delayedExecute(() => Animate.setGrey(E), .55);
+
+                Animate.delayedExecute(() => Animate.setRed(F), .5);
+                state[F.Name] = true;
+            };
+            /*
+            Animate.setRed(A);
+            Animate.delayedExecute(() => Animate.setGrey(A), .4);
+
+            Animate.delayedExecute(() => Animate.setRed(B), .3);
+            Animate.delayedExecute(() => Animate.setGrey(B), .4);
+
+            Animate.delayedExecute(() => Animate.setRed(C), .35);
+            Animate.delayedExecute(() => Animate.setGrey(C), .45);
+
+            Animate.delayedExecute(() => Animate.setRed(D), .4);
+            Animate.delayedExecute(() => Animate.setGrey(D), .5);
+
+            Animate.delayedExecute(() => Animate.setRed(E), .45);
+            Animate.delayedExecute(() => Animate.setGrey(E), .55);
+
+            Animate.delayedExecute(() => Animate.setRed(F), .5);
+            Animate.delayedExecute(() => Animate.setGrey(F), .6);
+            */
+        }
+    }
+}
+
--- Connect4/Connect4/MainWindow.xaml.cs
+++ Connect4/Connect4/MainWindow.xaml.cs
@@ -25,22 +25,40 @@ namespace Connect4
             InitializeComponent();
         }
 
+        /* Game state dictionary
+        * Key == slot position
+        * Value == occupied
+        */
+        private Dictionary<string, bool> gameState = new Dictionary<string, bool>
+        {
+            { "A1", false },{ "A2", false },{ "A3", false },{ "A4", false },{ "A5", false },{ "A6", false },{ "A7", false },
+            { "B1", false },{ "B2", false },{ "B3", false },{ "B4", false },{ "B5", false },{ "B6", false },{ "B7", false },
+            { "C1", false },{ "C2", false },{ "C3", false },{ "C4", false },{ "C5", false },{ "C6", false },{ "C7", false },
+            { "D1", false },{ "D2", false },{ "D3", false },{ "D4", false },{ "D5", false },{ "D6", false },{ "D7", false },
+            { "E1", false },{ "E2", false },{ "E3", false },{ "E4", false },{ "E5", false },{ "E6", false },{ "E7", false },
+            { "F1", false },{ "F2", false },{ "F3", false },{ "F4", false },{ "F5", false },{ "F6", false },{ "F7", false },
+
+        };
+
         private void button_A1_Click(object sender, RoutedEventArgs e)
         {
             Button A1 = sender as Button;
-            A1.Background = A1.Background == Brushes.Red ? (SolidColorBrush)
-                (new BrushConverter().ConvertFrom("SlateGray")): Brushes.Red;
 
+            Animate.dropPiece(A1, B1, C1, D1, E1, F1, gameState);
         }
 
         private void button_A2_Click(object sender, RoutedEventArgs e)
         {
+            Button A2 = sender as Button;
 
+            Animate.dropPiece(A2, B2, C2, D2, E2, F2, gameState);
         }
 
         private void button_A3_Click(object sender, RoutedEventArgs e)
         {
+            Button A3 = sender as Button;
 
+            Animate.dropPiece(A3, B3, C3, D3, E3, F3, gameState);
         }
 
         private void button_A4_Click(object sender, RoutedEventArgs e)
